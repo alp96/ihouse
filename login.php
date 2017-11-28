3f18beb032fcd35321649d228e8a66408f562e8f
H <hexbeastdev@gmail.com>
2017-11-28 09:27:19 +0100

    Debut

---
+++ login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html id='first_page'>
+<head>
+	<title>Se connecter - iHouse</title>
+	<link rel="stylesheet" href="style_login.css">
+	<link rel="icon" href="favicon.png" />
+	<meta charset="UTF-8">
+</head>
+<body>
+
+	<div class="wrap">
+		<img id="logo" src="iHouse.png">
+	</div>
+
+
+	<div class="wrap">
+		<form method="post" id='connexion' action="login.php">
+
+			<div id="titre">Espace personnel</div>
+
+			<div class="texte">Adresse e-mail</div>
+
+			<input class="field" type="text" name="mail">
+
+			<div class="texte">Mot de passe</div>
+
+			<input class="field" type="password" name="password">
+			<br>
+			<input id="bouton" type="submit" value="Se connecter">
+		</form>
+	</div>
+	<div class="wrap">
+		<div id="forget"><a class="link" href="password_lost.php">Identifiants oubliés ?</a></div>
+	</div>
+
+	<?php 
+
+	function verification($data){
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+
+		return $data;
+
+	}
+
+
+	if(isset($_POST["password"]) && isset($_POST["mail"]))
+	{
+		$_POST["mail"] = verification($_POST["mail"]);
+		$_POST["password"] = verification($_POST["password"]);
+
+		if($_POST["mail"] == '')
+		{
+			echo '<div class="error">Veuillez renseigner l\'adresse e-mail</div>';
+		}
+		elseif($_POST["password"] == '')
+		{
+			echo '<div class="error">Veuillez renseigner le mot de passe</div>';
+		}
+		elseif (!filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) 
+		{
+			echo '<div class="error">Veuillez rentrer une adresse mail valide</div>';
+		}
+		elseif ($_POST["mail"] != '' && $_POST["password"] != '')
+		{
+			if($_POST["mail"] != "test@test.com")
+			{
+				echo '<div class="error">Utilisateur inconnu</div>';
+			}
+			elseif($_POST["password"] != "juniorisep")
+			{
+				echo '<div class="error">Mauvais mot de passe</div>';
+			}
+			elseif($_POST["password"] == "juniorisep" && $_POST["mail"] == "test@test.com")
+			{
+				die("<script>location.href = 'https://www.ihouse-isep.com/app/test_00001/index.php'</script>");
+			}
+		}
+	}
+	?>
+
+</body>
+</html>
---
+++ password_lost.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html id='first_page'>
+<head>
+	<title>Mot de passe oublié - iHouse</title>
+	<link rel="stylesheet" href="style_login.css">
+	<link rel="icon" href="favicon.png" />
+	<meta charset="UTF-8">
+</head>
+<body>
+
+	<div class="wrap">
+		<img id="logo" src="iHouse.png">
+	</div>
+
+
+	<div class="wrap">
+		<form method="post" id='lostpassword' action="password_lost.php">
+
+			<div id="titre">Mot de passe oublié</div>
+
+			<div class="texte">Adresse e-mail</div>
+
+			<input class="field" type="text" name="mail">
+			<br>
+			<input id="bouton" type="submit" value="Générer un nouveau mot de passe">
+		</form>
+		
+		<div id="forget"><a class="link" href="login.php">Retour à l'écran de connexion</a></div>
+	</div>
+
+	<?php 
+	if(isset($_POST["mail"]))
+	{
+		if($_POST["mail"] == '')
+		{
+			echo '<div class="error">Veuillez renseigner l\'adresse e-mail</div>';
+		}
+		elseif (!filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) 
+		{
+			echo '<div class="error">Veuillez rentrer une adresse mail valide</div>';
+		}
+		else
+		{
+			echo '<div class="ok">Un lien de réinitialisation vous a été envoyé par email.</div>';
+
+		}
+	}
+
+	?>
+
+</body>
+</html>
