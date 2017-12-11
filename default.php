a854f82bc80c66d015e7115f9391b9dd41707fbe
Hexabeast <hexabeastdev@gmail.com>
2017-12-11 11:27:09 +0100

    Fonts & style

--- default.php
+++ default.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="style_login.css">
+	<link rel="stylesheet" href="style.css">
 	<link rel="icon" href="favicon.png" />
 	<meta charset="UTF-8">
 </head>
