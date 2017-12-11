dba702b02c705eb7554511fa17004d7074988aed
alp96 <annelouis.pinon@gmail.com>
2017-12-11 09:02:24 +0100

    je sais plus

--- login.php
+++ login.php
@@ -34,8 +34,23 @@
 	</div>
 
 	<?php 
+	$servername = "db701520246.db.1and1.com";
+	$username = "db701520246";
+	$password = "ihousebddISEP";
+	$bddname = "db701520246";
 
-	function verification($data){
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=$bddname;charset=utf8;", $username, $password);
+    // set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "Connected successfully"; 
+	}
+	catch(PDOException $e)
+	{
+		echo "Connection failed: " . $e->getMessage();
+	}
+
+		function verification($data){
 		$data = trim($data);
 		$data = stripslashes($data);
 		$data = htmlspecialchars($data);
