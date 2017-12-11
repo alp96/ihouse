a9d1d52d9fda863c270c80d304a1aeb9871e420c
alp96 <annelouis.pinon@gmail.com>
2017-12-11 22:17:47 +0100

    allez motivé !

--- template/header.php
+++ template/header.php
@@ -2,15 +2,17 @@
 		<img id="logo_mini" src="images/iHouse_logo_blanc.png">
 		<div class="info">
 			<img id="picture" src="images/man.png">
+
 			<?php 
 			include("template/connexionbdd.php");
-			$reponse = $bdd->query("SELECT * FROM Utilisateur WHERE mail='" . $_SESSION["mail"] . "'");
+			$reponse = $bdd->query("SELECT * FROM Utilisateur WHERE mail='" . $_SESSION["user"] . "'");
 			$donnees = $reponse->fetch();
 			$reponse->closeCursor();
 
 			$name = $donnees["nom"];
 			echo '<div id="username">Bonjour M. ' . $name . '</div>';
 			?>
+
 			<div id="ID">ID client : FR 09 67 34 2C</div>
 			<a href="#" id="logout">Se déconnecter</a>
 		</div>
