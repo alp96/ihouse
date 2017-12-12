67795bfe5421615bef0bd1ef86b229155bf81147
alp96 <annelouis.pinon@gmail.com>
2017-12-12 10:22:54 +0100

    t

--- create_user.php
+++ create_user.php
@@ -9,12 +9,12 @@
 <body>
 
 	<?php
-	/*session_start();
+	session_start();
 	session_regenerate_id();
 	if(!isset($_SESSION['user']))
 	{
 		die("<script>location.href = 'https://www.ihouse-panel.com/git/login.php'</script>");
-	}*/
+	}
 	include("template/connexionbdd.php");
 	include("template/header.php");
 	include("template/nav.php");
