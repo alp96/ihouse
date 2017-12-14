b3f21e4f62774561ebe21ce6a654dfbe252f11f3
alp96 <annelouis.pinon@gmail.com>
2017-12-14 21:24:29 +0100

    modif droit utilisateur

--- create_user.php
+++ create_user.php
@@ -16,42 +16,48 @@
 		die("<script>location.href = 'https://www.ihouse-panel.com/git/login.php'</script>");
 	}
 	include("template/header.php");
-	?>
-	<div id='wrap4'>
-		<?php
-		include("template/nav.php");
+	
+	if ($donnees['type_compte'] == "Administrateur") {
 		?>
+		<div id='wrap4'>
+			<?php
+			include("template/nav.php");
+			?>
 
-		<div id="content">
-			<form method="post" id='create_user' action="create_user_bdd.php">
+			<div id="content">
+				<form method="post" id='create_user' action="create_user_bdd.php">
 
-				<div id="titre">Création de compte</div>
+					<div id="titre">Création de compte</div>
 
-				<div class="texte">Nom</div>
-				<input class="field" type="text" name="name">
+					<div class="texte">Nom</div>
+					<input class="field" type="text" name="name">
 
-				<div class="texte">Prénom</div>
-				<input class="field" type="text" name="last_name">
+					<div class="texte">Prénom</div>
+					<input class="field" type="text" name="last_name">
 
-				<div class="texte">Adresse e-mail</div>
-				<input class="field" type="text" name="mail">
+					<div class="texte">Adresse e-mail</div>
+					<input class="field" type="text" name="mail">
 
-				<div class="texte">Numéro de téléphone</div>
-				<input class="field" type="text" name="phone">
+					<div class="texte">Numéro de téléphone</div>
+					<input class="field" type="text" name="phone">
 
-				<div class="texte">Type de compte</div>
-				<select name="account_type" size="1">
-					<option>Client</option>
-					<option>Technicien</option>
-					<option>Maintenance</option>
-					<option>Administrateur</option>
-				</select>
+					<div class="texte">Type de compte</div>
+					<select name="account_type" size="1">
+						<option>Client</option>
+						<option>Technicien</option>
+						<option>Maintenance</option>
+						<option>Administrateur</option>
+					</select>
 
-				<br>
-				<input id="bouton" type="submit" value="Créer un utilisateur">
-			</form>
+					<br>
+					<input id="bouton" type="submit" value="Créer un utilisateur">
+				</form>
+			</div>
 		</div>
-	</div>
-
+		<?php 
+	}
+	else{
+		die("<script>location.href = 'https://www.ihouse-panel.com/git/default.php'</script>");
+	}	?>
 </body>
 </html>
