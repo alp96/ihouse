b547d842e62bbb4e9934216ac18f4ace92660e96
alp96 <annelouis.pinon@gmail.com>
2017-12-14 22:33:19 +0100

    erreur

--- recherche_user.php
+++ recherche_user.php
@@ -4,7 +4,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	include("template/connexionbdd.php");
 
-	$nom = verification($_POST["champs_nom"])
+	$nom = verification($_POST["champs_nom"]);
 
 	if ($nom != "") {
 		$reponse = $bdd->query("SELECT * FROM Utilisateur WHERE nom LIKE '$nom'");
