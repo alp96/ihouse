dac752bae07ecf28b1c9381d55f2f211c7a0c635
alp96 <annelouis.pinon@gmail.com>
2017-12-14 22:45:49 +0100

    multi recherche

--- recherche_user.php
+++ recherche_user.php
@@ -4,16 +4,43 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	include("template/connexionbdd.php");
 
-	$nom = verification($_POST["champs_nom"]);
-
-	if ($nom != "") {
-		$reponse = $bdd->query("SELECT * FROM Utilisateur WHERE nom LIKE '$nom%'");
-		while ($donnees = $reponse->fetch())
-		{
-			echo $donnees['nom'];
-			echo '<br>';
+	if (isset($_POST["champs_nom"])) {
+		$nom = verification($_POST["champs_nom"]);
+		if ($nom != "") {
+			$reponse = $bdd->query("SELECT * FROM Utilisateur WHERE nom LIKE '$nom%'");
+			while ($donnees = $reponse->fetch())
+			{
+				echo $donnees['nom'];
+				echo '<br>';
+			}
+			$reponse->closeCursor();
 		}
-		$reponse->closeCursor();
+	}
+
+	if (isset($_POST["champs_prenom"])) {
+		$prenom = verification($_POST["champs_prenom"])
+		if ($prenom != "") {
+			$reponse = $bdd->query("SELECT * FROM Utilisateur WHERE prenom LIKE '$prenom%'");
+			while ($donnees = $reponse->fetch())
+			{
+				echo $donnees['prenom'];
+				echo '<br>';
+			}
+			$reponse->closeCursor();
+		}
+	}
+
+	if (isset($_POST["champs_mail"])) {
+		$mail = verification($_POST["champs_mail"])
+		if ($mail != "") {
+			$reponse = $bdd->query("SELECT * FROM Utilisateur WHERE mail LIKE '$mail%'");
+			while ($donnees = $reponse->fetch())
+			{
+				echo $donnees['mail'];
+				echo '<br>';
+			}
+			$reponse->closeCursor();
+		}
 	}
 }
 
