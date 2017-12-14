2f80e760a1c294465e0e00e472e7ebfe4a93a12b
alp96 <annelouis.pinon@gmail.com>
2017-12-14 22:32:22 +0100

    test

---
+++ recherche_user.php
@@ -0,0 +1,21 @@
+<?php 
+
+if($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+	include("template/connexionbdd.php");
+
+	$nom = verification($_POST["champs_nom"])
+
+	if ($nom != "") {
+		$reponse = $bdd->query("SELECT * FROM Utilisateur WHERE nom LIKE '$nom'");
+		$donnees = $reponse->fetch();
+		while ($donnees = $reponse->fetch())
+		{
+			echo $donnees['nom'];
+			echo '<br>';
+		}
+		$reponse->closeCursor();
+	}
+}
+
+?>
