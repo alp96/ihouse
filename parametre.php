0b60f62ecf869ba3c78b5fbfb696644474a67504
alp96 <annelouis.pinon@gmail.com>
2017-12-15 12:53:29 +0100

    ajout modif mdp

--- parametre.php
+++ parametre.php
@@ -42,7 +42,7 @@
 					<br>
 					<input id="bouton" type="submit" value="Modifier Email">
 				</form>
-				<form method="post" id='modif_email' action="parametre.php">
+				<form method="post" id='modif_phone' action="parametre.php">
 					<div class="texte">Numéro de téléphone actuel :
 						<br>
 						<?php echo $donnees["telephone"]; ?>
@@ -51,16 +51,28 @@
 					<br>
 					<input id="bouton" type="submit" value="Modifier le numéro">
 				</form>
+				<form method="post" id='modif_mdp' action="parametre.php">
+					<div class="texte">Mot de passe :</div>
+
+					<input class="field" type="password" name="mdp_old" placeholder="Ancien mot de passe">
+					<br>
+					<input class="field" type="password" name="mdp1" placeholder="Nouveau mot de passe">
+					<br>
+					<input class="field" type="password" name="mdp2" placeholder="Confirmer mot de passe">
+					<br>
+					<input id="bouton" type="submit" value="Modifier mot de passe">
+				</form>
 			</div>
 		</div>
 	</div>
 	<?php
 	if($_SERVER["REQUEST_METHOD"] == "POST") 
 	{
-		if(isset($_POST["phone"]) OR isset($_POST["mail"])) 
+		if(isset($_POST["phone"]) OR isset($_POST["mail"]) OR (isset($_POST["mdp_old"]) AND isset($_POST["mdp1"]) AND isset($_POST["mdp2"])))
 		{
 			$mail = $donnees["mail"];
-			if (isset($_POST["mail"])) {
+			if (isset($_POST["mail"])) 
+			{
 				$_POST["mail"] = verification($_POST["mail"]);
 				if($_POST["mail"] == '')
 				{
@@ -70,7 +82,8 @@
 				{
 					echo '<div class="error">Veuillez rentrer une adresse mail valide</div>';
 				}
-				elseif ($_POST["mail"] != '') {
+				elseif ($_POST["mail"] != '') 
+				{
 					$new_mail = $_POST["mail"];
 					$bdd->exec("UPDATE Utilisateur SET mail = '$new_mail' WHERE mail = '$mail'");
 					$reponse = $bdd->query("SELECT * FROM Utilisateur WHERE mail='$new_mail'");
@@ -79,21 +92,46 @@
 					$_SESSION['user'] = $new_mail;
 				}
 			}
-			if (isset($_POST["phone"])) {
+			if (isset($_POST["phone"])) 
+			{
 				$_POST["phone"] = verification($_POST["phone"]);
 				if($_POST["phone"] == '')
 				{
 					echo '<div class="error">Veuillez renseigner votre numéro</div>';
 				}
-				else{
+				else
+				{
 					$new_phone = $_POST["phone"];
 					$bdd->exec("UPDATE Utilisateur SET telephone = '$new_phone' WHERE mail = '$mail'");
 				}
 			}
+			if (isset($_POST["mdp_old"]) AND isset($_POST["mdp1"]) AND isset($_POST["mdp2"])) 
+			{
+				$_POST["mdp_old"] = verification($_POST["mdp_old"]);
+				$_POST["mdp1"] = verification($_POST["mdp1"]);
+				$_POST["mdp2"] = verification($_POST["mdp2"]);
+				$options = ['cost' => 11,];
 
+				if($_POST["mdp_old"] == '' OR $_POST["mdp1"] == '' OR $_POST["mdp2"] == '')
+				{
+					echo '<div class="error">Veuillez renseigner vos mots de passe</div>';
+				}
+				elseif (password_verify($_POST["mdp_old"], $donnees["password"]) == FALSE)
+				{
+					echo '<div class="error">Mot de passe erroné</div>';
+				}				}
+				elseif ($_POST["mdp1"] != $_POST["mdp2"]) 
+				{
+					echo '<div class="error">Vos nouveaux mots de passe ne sont pas identiques</div>';
+				}
+				elseif ($_POST["mdp1"] == $_POST["mdp2"] AND password_verify($_POST["mdp_old"], $donnees["password"]))
+				{
+					$new_password = password_hash($_POST["mdp1"], PASSWORD_BCRYPT, $options);
+					$bdd->exec("UPDATE Utilisateur SET password = '$new_password' WHERE mail = '$mail'");
+				}
+			}
 		}
 	}
 	?>
-
 </body>
 </html>
