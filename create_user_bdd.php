924c5f7e0bc360b8264afdbe40f532338a9956a1
alp96 <annelouis.pinon@gmail.com>
2017-12-22 17:04:00 +0100

    erreur création utilisateur vide

--- create_user_bdd.php
+++ create_user_bdd.php
@@ -4,22 +4,28 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	if (isset($_POST["name"]) AND isset($_POST["last_name"]) AND isset($_POST["mail"]) AND isset($_POST["phone"]) AND isset($_POST["account_type"]) AND isset($_POST["genre"])) {
 
-		include("template/connexionbdd.php");
+		if ($_POST["name"] != "" AND $_POST["last_name"] != "" AND $_POST["mail"] != "" AND $_POST["phone"] != ""){
+			include("template/connexionbdd.php");
 
-		$name = verification($_POST["name"]);
-		$genre = verification($_POST["genre"]);
-		$last_name = verification($_POST["last_name"]);
-		$mail = verification($_POST["mail"]);
-		$phone = verification($_POST["phone"]);
-		$account_type = verification($_POST["account_type"]);
-		$options = ['cost' => 11,];
-		$password = password_hash("test", PASSWORD_BCRYPT, $options);
-		$id_house = '0';
+			$name = verification($_POST["name"]);
+			$genre = verification($_POST["genre"]);
+			$last_name = verification($_POST["last_name"]);
+			$mail = verification($_POST["mail"]);
+			$phone = verification($_POST["phone"]);
+			$account_type = verification($_POST["account_type"]);
+			$options = ['cost' => 11,];
+			$password = password_hash("test", PASSWORD_BCRYPT, $options);
+			$id_house = '0';
 
-		$bdd->exec("INSERT INTO Utilisateur(id_utilisateur, nom, prenom, genre, mail, password, telephone, type_compte, id_maison) VALUES (DEFAULT, '$name', '$last_name', '$genre','$mail', '$password', '$phone', '$account_type', '$id_house')");
+			$bdd->exec("INSERT INTO Utilisateur(id_utilisateur, nom, prenom, genre, mail, password, telephone, type_compte, id_maison) VALUES (DEFAULT, '$name', '$last_name', '$genre','$mail', '$password', '$phone', '$account_type', '$id_house')");
 
-		die("<script>location.href = 'https://www.ihouse-panel.com/git/create_user.php'</script>");
-
+			die("<script>location.href = 'https://www.ihouse-panel.com/git/create_user.php'</script>");
+		}
+		else
+		{
+			echo '<div class="error">Champ vide !</div>';
+			die("<script>location.href = 'https://www.ihouse-panel.com/git/create_user.php'</script>");
+		}
 	}
 }
 ?>
