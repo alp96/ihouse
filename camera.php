366acd4b72849c87c492fb81f09715b665ede053
alp96 <annelouis.pinon@gmail.com>
2018-01-23 13:14:47 +0100

    css

--- camera.php
+++ camera.php
@@ -34,7 +34,7 @@
 			$reponse = $bdd->query("SELECT * FROM camera WHERE id_utilisateur='" . $id_user . "'");
 			while($donnees = $reponse->fetch())
 			{
-				echo '<iframe width="560" height="315" src="' . $donnees["url"] . '" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>';
+				echo '<div class="camera"><iframe width="560" height="315" src="' . $donnees["url"] . '" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>';
 			}
 			$reponse->closeCursor();
 
