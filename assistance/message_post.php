7b7c14f458fd3f14b20c628d9d9d7b1c3a975889
TartifletteOFromage <34056210+TartifletteOFromage@users.noreply.github.com>
2018-02-01 23:42:09 +0100

    Update message_post.php

--- assistance/message_post.php
+++ assistance/message_post.php
@@ -6,9 +6,10 @@
 
 
 	/*Insertion du message à l'aide d'une requête préparée*/
+	if(isset($_SESSION['dernier_ticket_consulte']) && $_SESSION['dernier_ticket_consulte']!=0)
+	{
+		$req = $bdd->prepare('INSERT INTO message (id_ticket,id_utilisateur,message) VALUES(?,?,?)');
 
-	$req = $bdd->prepare('INSERT INTO message (id_ticket,id_utilisateur,message) VALUES(?,?,?)');
-
-	$req->execute(array($_SESSION['dernier_ticket_consulte'],$_SESSION['id_utilisateur'],$_POST['message']));	
-	
+		$req->execute(array($_SESSION['dernier_ticket_consulte'],$_SESSION['id_utilisateur'],$_POST['message']));	
+	}
 ?>
