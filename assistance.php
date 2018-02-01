e1bcaea56b9819e19f52ef6307c893780a42f9e7
TartifletteOFromage <34056210+TartifletteOFromage@users.noreply.github.com>
2018-02-01 15:36:03 +0100

    Update assistance.php

--- assistance.php
+++ assistance.php
@@ -44,8 +44,6 @@
 			border-radius: 5px;
 			border: 0px;
 			margin: 10px;
-			margin-top: 30px;
-			margin-bottom: 15px;
 			padding: 5px 10px;
 			box-shadow: 2px 2px 20px #1f1f4a;
 			font-size: 16px;
