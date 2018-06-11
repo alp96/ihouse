0aac4e2792d583c94fda7a957c82b9d2c9964342
alp96 <annelouis.pinon@gmail.com>
2018-06-11 11:31:37 +0200

    change title and width

--- stats.php
+++ stats.php
@@ -116,7 +116,7 @@ function drawChart() {
 		]);
 
   // Optional; add a title and set the width and height of the chart
-  var options = {'title':'My Average Day', 'width':550, 'height':400};
+  var options = {'title':'Humidité dans les salles', 'width':70%, 'height':50%};
 
   // Display the chart inside the <div> element with id="piechart"
   var chart = new google.visualization.PieChart(document.getElementById('piechart'));
