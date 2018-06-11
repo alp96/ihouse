2a95e8ff57511879d0ad0f2a0ebdbc63cc0c2172
alp96 <annelouis.pinon@gmail.com>
2018-06-11 10:27:47 +0200

    modif chart

--- stats.php
+++ stats.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION['user']))
 	<meta charset="UTF-8">
 	<title>Statistiques</title>
 
-	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+	<!--<script type="text/javascript" src="https://www.google.com/jsapi"></script>
 
 	<script>
 		google.charts.load('current', '1', {packages: ['corechart', 'bar']});
@@ -85,11 +85,43 @@ if(!isset($_SESSION['user']))
 			chart.draw(data, options);
 		}
 
-	</script>
+	</script>-->
 
 	<?php
 	include("template/header.php");
 	?>
 
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-	<div id="chart_div"></div>
+<h1>My Web Page</h1>
+
+<div id="piechart"></div>
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<script type="text/javascript">
+// Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['Work', 8],
+  ['Eat', 2],
+  ['TV', 4],
+  ['Gym', 2],
+  ['Sleep', 8]
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'title':'My Average Day', 'width':550, 'height':400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
+</script>
+
+
+	<!--<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<div id="chart_div"></div>-->
