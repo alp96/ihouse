95825d60bac78985b53bf10dab873a53155078c5
alp96 <annelouis.pinon@gmail.com>
2018-06-25 09:01:03 +0200

    test

--- stats.php
+++ stats.php
@@ -18,7 +18,8 @@ if(!isset($_SESSION['user']))
 	<?php
 	include("template/header.php");
 	?>
-
+</head>
+<body>
 	<div id='wrap4'>
 		<div id="content">
 
@@ -67,51 +68,63 @@ if(!isset($_SESSION['user']))
 
 			</form>
 
+			<?php 
+			$titre = "";
+			if ((!empty($_POST["genre"]) && !empty($_POST["choix"]))) {
+				if ($_POST["genre"] == "Temperature") {
+					$resultat = "Temperature";
+					$titre = "Température";
+					$titre_colonne2 = "°C";
+
+				}
+			}
+			 ?>
 
 			<div id="piechart"></div>
 
 			<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
 			<script type="text/javascript">
-// Load google charts 
-google.charts.load('current', {packages: ['corechart', 'line']});
-google.charts.setOnLoadCallback(drawChart);
+				// Load google charts 
+				google.charts.load('current', {packages: ['corechart', 'line']});
+				google.charts.setOnLoadCallback(drawChart);
 
-// Draw the chart and set the chart values
-function drawChart() {
-	var data = new google.visualization.DataTable();
-	data.addColumn('number', 'X');
-	data.addColumn('number', 'Dogs');
+				// Draw the chart and set the chart values
+				function drawChart() {
+					var data = new google.visualization.DataTable();
+					data.addColumn('number', 'X');
+					data.addColumn('number', 'Dogs');
 
-	data.addRows([
-		[0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
-		[6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
-		[12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-		[18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-		[24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-		[30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-		[36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-		[42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-		[48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-		[54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-		[60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-		[66, 70], [67, 72], [68, 75], [69, 80]
-		]);
+					data.addRows([
+						[0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+						[6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+						[12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+						[18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+						[24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+						[30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+						[36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+						[42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+						[48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+						[54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+						[60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+						[66, 70], [67, 72], [68, 75], [69, 80]
+						]);
 
-  // Optional; add a title and set the width and height of the chart
-  var options = {
-  	hAxis: {
-  		title: 'TitreCapteur'
-  	},
-  	vAxis: {
-  		title: 'Popularity'
-  	}
-  };
+				  // Optional; add a title and set the width and height of the chart
+				  var options = {
+				  	hAxis: {
+				  		title: '<?php echo $titre; ?>'
+				  	},
+				  	vAxis: {
+				  		title: 'Popularity'
+				  	}
+				  };
 
-  // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.LineChart(document.getElementById('piechart'));
-  chart.draw(data, options);
-}
-</script>
-</div>
-</div>
+				  // Display the chart inside the <div> element with id="piechart"
+				  var chart = new google.visualization.LineChart(document.getElementById('piechart'));
+				  chart.draw(data, options);
+				}
+			</script>
+		</div>
+	</div>
+</body>
